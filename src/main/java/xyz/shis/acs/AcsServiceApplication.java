b3f0e61c68a4afb15ba36f843577919bc20ec03d
shis <shis@shis95.vip>
2022-06-30 15:10:49 +0800

    init

---
+++ src/main/java/xyz/shis/acs/AcsServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.shis.acs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/shis/acs/AcsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.shis.acs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
